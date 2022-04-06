39840b8920ed77e970345ed84194a63836e18ff1
yk <yosizuka1@gmail.com>
2022-04-06 11:50:51 +0900

    stg

--- lib/Beauth/Build.pm
+++ lib/Beauth/Build.pm
@@ -13,14 +13,30 @@ sub start {
     return $self->error->commit("No arguments") if !$opt;
 
     # 初期設定時のdbファイル準備
-    return $self->_init()       if $opt->{method} eq 'init';
+    return $self->_init($opt)   if $opt->{method} eq 'init';
     return $self->_insert($opt) if $opt->{method} eq 'insert';
     return $self->_dump()       if $opt->{method} eq 'dump';
     return $self->_restore()    if $opt->{method} eq 'restore';
     return $self->error->commit(
         "Method not specified correctly: $opt->{method}");
 }
-sub _init    { shift->db->build(); }
+
+sub _init {
+    my ( $self, @args ) = @_;
+    my $opt    = shift @args;
+    my $params = $opt->{params};
+    if ( exists $params->{name} ) {
+        my $name = $params->{name};
+        my $path = File::Spec->catfile( $self->homedb(), $name );
+        if ( $ENV{"BEAUTH_MODE"} && ( $ENV{"BEAUTH_MODE"} eq 'test' ) ) {
+            my $test_dir = $ENV{"BEAUTH_TESTDIR"};
+            $path = File::Spec->catfile( $test_dir, $name );
+        }
+        my $args = { db_file_path => $path, };
+        return $self->db($args)->build();
+    }
+    return $self->db->build();
+}
 sub _dump    { shift->db->build_dump(); }
 sub _restore { shift->db->build_restore(); }
 
